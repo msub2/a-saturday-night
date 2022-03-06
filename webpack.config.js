f65627893b1d0843bea324d96e6d6f997ad1edd7
Daniel Adams <msub2official@gmail.com>
2022-03-05 20:51:00 -0500

    Update dependencies and build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,15 @@
-var webpack = require('webpack');
-
-var PLUGINS = [];
-if (process.env.NODE_ENV === 'production') {
-  PLUGINS.push(new webpack.optimize.UglifyJsPlugin());
-}
-
 module.exports = {
   entry: './src/index.js',
   output: {
     path: __dirname,
     filename: 'build/build.js'
   },
-  plugins: PLUGINS,
   devServer: {
-    disableHostCheck: true
+    historyApiFallback: true,
+    allowedHosts: "all",
+    static: {
+      directory: __dirname,
+      publicPath: '/'
+    }
   }
 };
